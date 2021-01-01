fd973bde02c241a99b44f76468a3cdb34a3a78a4
Anas Yaseen <Anasyaseen1@outlook.com>
2021-01-01 23:42:48 +0200

    1.0 Basic HTML and SCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus, faEdit } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <h2>BlueDoc</h2>
+          <p>Your healthy solution</p>
+        </header>
+        <main>
+          <h3>Book an appointment right now</h3>
+          <form>
+            <label htmlFor="name">Name</label>
+
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your name"
+              required
+            />
+            <label htmlFor="date" required>
+              Date
+            </label>
+            <input type="date" id="date" name="date" />
+
+            <label htmlFor="time" required>
+              Time
+            </label>
+            <input type="time" id="time" name="time" />
+            <FontAwesomeIcon
+              title="Add Appointment"
+              className="add"
+              icon={faPlus}
+            />
+          </form>
+        </main>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
