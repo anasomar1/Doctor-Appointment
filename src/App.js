8c13a1c4528fc2e2fe6b1e85a1159ef845da1136
Anas Yaseen <Anasyaseen1@outlook.com>
2021-01-02 10:22:39 +0200

    1.5.1 Minor edits

--- src/App.js
+++ src/App.js
@@ -64,76 +64,77 @@ const App = () => {
     <div className="App">
       <header>
         <h2>BlueDoc</h2>
-        <p>Your healthy solution</p>
       </header>
-      <main>
-        <div className="appointment">
-          <FontAwesomeIcon
-            title="Add Appointment"
-            className="add"
-            icon={isToggled ? faMinus : faPlus}
-            onClick={() => setIsToggle(!isToggled)}
-          />
-          <h3>Book an appointment!</h3>
-        </div>
-        {isToggled && (
-          <form onSubmit={submitHandler}>
-            <label htmlFor="name">Name</label>
+      <div className="container">
+        <main>
+          <div className="appointment">
+            <FontAwesomeIcon
+              title="Add Appointment"
+              className="add"
+              icon={isToggled ? faMinus : faPlus}
+              onClick={() => setIsToggle(!isToggled)}
+            />
+            <h3>Book an appointment!</h3>
+          </div>
+          {isToggled && (
+            <form onSubmit={submitHandler}>
+              <label htmlFor="name">Name</label>
 
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              value={name}
-              onChange={inputHandler}
-              required
-            />
-            <label htmlFor="date" required>
-              Date
-            </label>
-            <input
-              value={date}
-              type="date"
-              id="date"
-              name="date"
-              onChange={inputHandler}
-              required
-            />
-
-            <label htmlFor="time" required>
-              Condition
-            </label>
-            <input
-              type="text"
-              id="condition"
-              value={condition}
-              name="condition"
-              onChange={inputHandler}
-              required
-            />
-            <button className="btn" type="submit">
-              Add appointment
-            </button>
-          </form>
-        )}
-        <ul>
-          {appointments.map((appointment) => {
-            const { name, date, condition, id } = appointment;
-            return (
-              <ListItem
-                name={name}
-                date={date}
-                condition={condition}
-                id={id}
-                key={id}
-                deleteHandler={deleteHandler}
-                editHandler={editHandler}
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Your name"
+                value={name}
+                onChange={inputHandler}
+                required
               />
-            );
-          })}
-        </ul>
-      </main>
+              <label htmlFor="date" required>
+                Date
+              </label>
+              <input
+                value={date}
+                type="date"
+                id="date"
+                name="date"
+                onChange={inputHandler}
+                required
+              />
+
+              <label htmlFor="time" required>
+                Condition
+              </label>
+              <input
+                type="text"
+                id="condition"
+                value={condition}
+                name="condition"
+                onChange={inputHandler}
+                required
+              />
+              <button className="btn" type="submit">
+                Add appointment
+              </button>
+            </form>
+          )}
+          <ul>
+            {appointments.map((appointment) => {
+              const { name, date, condition, id } = appointment;
+              return (
+                <ListItem
+                  name={name}
+                  date={date}
+                  condition={condition}
+                  id={id}
+                  key={id}
+                  deleteHandler={deleteHandler}
+                  editHandler={editHandler}
+                />
+              );
+            })}
+          </ul>
+        </main>
+      </div>
     </div>
   );
 };
